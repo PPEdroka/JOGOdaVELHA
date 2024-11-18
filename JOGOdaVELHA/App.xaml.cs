5dc48fcb673f1502d6a68fe3c5d49a6ad5c51d0a
Admin <Admin@DESKTOP-2KP0K61>
2024-11-18 02:22:46 -0300

    Adicionar arquivos de projeto.

---
+++ JOGOdaVELHA/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JOGOdaVELHA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
